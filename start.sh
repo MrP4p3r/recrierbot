4f5a12c4b9a3792ab6a279aacd8b54f1e6849673
Gleb G <mrp4p3r@gmail.com>
2018-03-17 19:20:08 +0300

    creating db directory correctly

--- start.sh
+++ start.sh
@@ -19,7 +19,9 @@ export THREADS_NUMBER=${THREADS_NUMBER:-4}
 export TORNADO_PORT=${TORNADO_PORT:-8080}
 
 
-mkdir -p $(dirname $DB_PATH)
+if [ ! -d "$(dirname "$DB_PATH")" ]; then
+    mkdir -p $(dirname $DB_PATH) || true
+fi
 
 
 /main
