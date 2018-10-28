88e1f3fcc1077ea14154cc33a2e373fd3b1c44a0
Gleb G <mrp4p3r@gmail.com>
2018-10-28 15:36:48 +0300

    debug: removed temporary debuginfo output

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from recrierbot.__main__ import main
 
 
-def debuginfo():
-    import sys
-    import asyncio
-    print(sys.version, file=sys.stderr)
-    print(dir(asyncio), file=sys.stderr)
-
-
 if __name__ == '__main__':
-    debuginfo()
     main()
